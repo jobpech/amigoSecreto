6f6bfec96ba7ed5660eb1541ed83d3d861931161
jobpech <jobpech@gmail.com>
2025-03-07 11:40:44 -0600

    Se agregaron validaciones, correccion de codigo

--- app.js
+++ app.js
@@ -32,7 +32,8 @@ function limpiarCaja(){
  //Funcion agrega nombre de amigo al arreglo
 function agregarAmigo(){
    
-    capturaAmigo = document.getElementById("amigo").value;
+    capturaAmigo = document.getElementById("amigo").value.toUpperCase();
+    
 
     if (capturaAmigo){
         
@@ -43,7 +44,7 @@ function agregarAmigo(){
         return;
 
     }else{
-        alert("Favor de capturar nombre correcto ejemplo: Pedro");
+        alertNombreCorrecto();
         return;
 
     }
@@ -52,22 +53,26 @@ function agregarAmigo(){
 //Funcion sortea amigo
 function sortearAmigo() {
     let numeroMaximo = amigos.length; 
-    let numeroSorteado = Math.floor(Math.random()*numeroMaximo)+1;
+    let numeroSorteado = Math.floor(Math.random()*numeroMaximo);
+    console.log(numeroSorteado);
 
     if(amigos.length >= 2){
         let lista = document.getElementById("resultado");
         lista.innerHTML = ""; //Limpiar lista
         let li = document.createElement("li"); //crear lista
-        li.textContent = amigos[numeroSorteado]; //colocar texto
+        li.textContent = `Tu amigo Secreto es: ${amigos[numeroSorteado]}`; //colocar texto 
         lista.appendChild(li);//agregar a lista ul
         document.getElementById("reiniciar").disabled = false; 
-        
+        document.getElementById("sortear").disabled = true;
+        document.getElementById("amigo").disabled = true;
+        document.getElementById("add").disabled = true;
         return;
+
     }if (amigos.length >= 1) {
-        alert("Favor de agregar mas de un amigo a la lista");
+        alertAgregarMasAmigos();
         return;
     } else {
-        alert("Favor de agregar amigos a la lista para sortar");
+        alertAgregarAmigo();
         return;
     }
 
@@ -81,9 +86,60 @@ function reiniciarJuego(){
     lista2.innerHTML = ""; //Limpiar lista
     amigos = [];
     document.getElementById("reiniciar").disabled = true;
-    alert ("Juego Reiniciado");
+    document.getElementById("sortear").disabled = false;
+    document.getElementById("amigo").disabled = false;
+    document.getElementById("add").disabled = false;
+    alertReiniciar()
     return
-    
+}
 
+//alert personalizado
+
+function alertReiniciar() {
+    Swal.fire({
+        title: '¡juego Reiniciado!',
+        text: 'Gracias por jugar',
+        icon: 'success', // Puedes cambiar el ícono (success, error, warning, info, question)
+        confirmButtonText: 'Aceptar',
+        customClass: {
+            popup: 'mi-alerta', // Clase CSS personalizada
+        },
+    });
+}
+
+function alertAgregarAmigo() {
+    Swal.fire({
+        title: '¡Disculpa!',
+        text: 'No tienes agregado ningun amigo a la lista',
+        icon: 'info', // Puedes cambiar el ícono (success, error, warning, info, question)
+        confirmButtonText: 'Aceptar',
+        customClass: {
+            popup: 'mi-alerta', // Clase CSS personalizada
+        },
+    });
+}
+
+function alertAgregarMasAmigos() {
+    Swal.fire({
+        title: '¡Oye faltan amigos!',
+        text: 'Agrega por lo menos 2 amigos para sortear',
+        icon: 'question', // Puedes cambiar el ícono (success, error, warning, info, question)
+        confirmButtonText: 'Aceptar',
+        customClass: {
+            popup: 'mi-alerta', // Clase CSS personalizada
+        },
+    });
+}
+
+function alertNombreCorrecto() {
+    Swal.fire({
+        title: '¡Disculpa!',
+        text: 'Favor de capturar el nombre de tus amigos o en su defecto esposa XD Ej: Liliana, Belinda etc.',
+        icon: 'Warning', // Puedes cambiar el ícono (success, error, warning, info, question)
+        confirmButtonText: 'Aceptar',
+        customClass: {
+            popup: 'mi-alerta', // Clase CSS personalizada
+        },
+    });
 }
 
